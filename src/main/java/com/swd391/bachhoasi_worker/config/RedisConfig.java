dcf86e7936ac54bf01662a597044528a1bc8827b
AnataArisa <92080339+AnataAria@users.noreply.github.com>
2024-06-25 21:11:06 +0700

    reconfig redis

--- src/main/java/com/swd391/bachhoasi_worker/config/RedisConfig.java
+++ src/main/java/com/swd391/bachhoasi_worker/config/RedisConfig.java
@@ -1,11 +1,10 @@
 package com.swd391.bachhoasi_worker.config;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.event.EventListener;
 
+import jakarta.annotation.PostConstruct;
 import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisPool;
 
@@ -17,8 +16,8 @@ public class RedisConfig {
     private Integer redisPort;
     private JedisPool pool;
 
-    @EventListener
-    private void initRedisConnection(ApplicationReadyEvent event) {
+    @PostConstruct
+    private void initRedisConnection() {
         pool = new JedisPool(redisUrl, redisPort);
     }
     @Bean
